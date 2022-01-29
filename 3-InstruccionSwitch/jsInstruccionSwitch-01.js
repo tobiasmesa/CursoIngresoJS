72594b9fd1729ca7564728c9a53393ad05af8269
tobias <tobiasmesa030303@gmail.com>
2022-01-29 18:57:22 -0300

    Switch - Ej 1. solved

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,8 +1,21 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	var mes= document.getElementById("txtIdMes").value;
+	switch (mes){
+		case "Enero": 
+			alert("que comiences bien el año!!!.");
+			break;
+		case "Marzo": 
+			alert("a clases!!!.");
+			break;
+		case "Julio": 
+			alert("se vienen las vacaciones!!!");
+			break;
+		case "Diciembre": 
+			alert("Felices fiestas!!!");
+			break;
+	}
 
 
 
