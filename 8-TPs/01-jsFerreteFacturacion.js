f72d6faaa5723eb980f6be17c7b690af7de45147
tobias <tobiasmesa030303@gmail.com>
2022-01-29 15:06:21 -0300

    TP 1 - Ferrete Facturación (E/S) Solved

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,30 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	let numUno = parseFloat(document.getElementById("txtIdPrecioUno").value);
+    let numDos = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    let numTres = parseFloat(document.getElementById("txtIdPrecioTres").value);
+    let suma = numUno + numDos + numTres;
+
+    alert("La suma es: $" + suma);
 }
 function Promedio () 
 {
-	
+	let numUno = parseFloat(document.getElementById("txtIdPrecioUno").value);
+    let numDos = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    let numTres = parseFloat(document.getElementById("txtIdPrecioTres").value);
+    let promedio = ((numUno + numDos + numTres) / 3);
+
+    alert("El promedio es: $" + promedio.toFixed(2));
 }
 function PrecioFinal () 
 {
 	
+	let numUno = parseFloat(document.getElementById("txtIdPrecioUno").value);
+    let numDos = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    let numTres = parseFloat(document.getElementById("txtIdPrecioTres").value);
+    let suma = numUno + numDos + numTres;
+    let precioFinal = (suma * 0.21) + suma;
+
+    alert("El precio final es: $" + precioFinal.toFixed(2));
 }
