6651e41babe74e9e8fa912e23d429d912c9a9cae
tobias <tobiasmesa030303@gmail.com>
2022-01-29 13:10:04 -0300

    EntradaSalida - Ej.8 solved

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -5,5 +5,8 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	var dividendo = parseInt(document.getElementById("txtIdNumeroDividendo").value);
+	var divisor = parseInt(document.getElementById("txtIdNumeroDivisor").value);
+	let resto = dividendo % divisor;
+	alert("El resto es: " + resto);
 }
