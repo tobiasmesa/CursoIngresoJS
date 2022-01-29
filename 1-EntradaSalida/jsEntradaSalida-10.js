98c421473e15eb1862766e4c5ff59b200de7d9ed
tobias <tobiasmesa030303@gmail.com>
2022-01-29 13:28:07 -0300

    EntradaSalida - Ej.10 solved

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,7 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	let importe = parseInt(document.getElementById("txtIdImporte").value);
+	let resultado = importe - (importe * 0.25); 
+	document.getElementById("txtIdResultado").value = resultado;
 }
