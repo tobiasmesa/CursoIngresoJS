0704020df0773c12e8edd4be6743ef954fa31382
tobias <tobiasmesa030303@gmail.com>
2022-01-29 17:41:26 -0300

    IF - Ej. 9 and 10 solved

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,11 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	let min = 1; 
+	let max = 10;
+
+	let num = Math.round(Math.random() * (max - min) + min);
+	
+	alert(num);	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let min = 1; 
+	let max = 10;
+
+	let nota = Math.round(Math.random() * (max - min) + min);
+
+	if(nota >= 9){
+		alert("Excelente nota: " + nota);
+	} else if (nota >= 4){
+		alert("Aprobó Nota: " + nota);
+	} else {
+		alert("Vamos, la proxima se puede. Nota: " + nota) ;
+	}
 
 }//FIN DE LA FUNCIÓN
