ab48dece03067a1adec61c27a1633586bca69a8c
tobias <tobiasmesa030303@gmail.com>
2022-01-29 12:32:36 -0300

    EntradaSalida - Ej.4 solved

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,8 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let nombreIngresado = prompt("Ingrese nombre"); 
+	document.getElementById("txtIdNombre").value = nombreIngresado; 
+
 }
 
