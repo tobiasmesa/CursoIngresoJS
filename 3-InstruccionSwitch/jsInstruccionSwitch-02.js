93c6379aa3b32132a9b561a9e702d6f32c2850de
tobias <tobiasmesa030303@gmail.com>
2022-01-29 19:07:24 -0300

    Switch - Ej. 2 solved

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,25 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-
-
-
+	var mes= document.getElementById("txtIdMes").value;
+	switch (mes){
+		case "Marzo": 
+		case "Abril": 
+		case "Mayo": 
+		case "Junio": 
+			alert("Falta para el invierno.");
+			break;
+		case "Julio": 
+		case "Agosto": 
+			alert("Abrigate que hace frio");
+			break;
+		case "Septiembre": 
+		case "Octubre": 
+		case "Noviembre": 
+		case "Diciembre": 
+		case "Enero": 
+		case "Febrero":  
+			alert("Ya pasamos el frio, ahora calor!!!.");
+			break;
+	}
 
 }//FIN DE LA FUNCIÓN
