b807da1a5e573e0c698c3a5ac5221357c4afa706
tobias <tobiasmesa030303@gmail.com>
2022-01-30 19:28:21 -0300

    Switch - Ej. 04 solved

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,9 +1,29 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
+	let mes = document.getElementById("txtIdMes").value;
+	/* al seleccionar un mes informar.
+si tiene 28 días.
+si tiene 30 días.
+si tiene 31 días.*/
+
+	switch(mes){ 
+		case "Enero":
+		case "Marzo":
+		case "Mayo": 
+		case "Julio": 
+		case "Agosto": 
+		case "Octubre": 
+		case "Diciembre":
+			alert("Tiene 31 días.");
+			break; 
+		case "Febrero": 
+			alert("Tiene 28 días.");
+			break; 
+		default:
+			alert("Tiene 30 días."); 
+			break;
+	}
 	
 
 
