86d6f91d5826900294048aee42bc9118b1792769
tobias <tobiasmesa030303@gmail.com>
2022-01-30 19:47:52 -0300

    Switch - Ej. 7 and 8 solved

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,22 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	var destinoIngresado = document.getElementById("txtIdDestino").value;
+	let puntoCardinal;
+	
+	switch (destinoIngresado){ 
+		case "Bariloche": 
+			puntoCardinal = "Oeste";
+			break;
+		case "Cataratas":
+			puntoCardinal = "Norte";
+			break;
+		case "Mar del plata":
+			puntoCardinal = "Este";
+			break;
+		case "Ushuaia":
+			puntoCardinal = "Sur";
+			break;
+	}
 
+	alert("La ubicación ingresa queda en el " + puntoCardinal);
 }//FIN DE LA FUNCIÓN
