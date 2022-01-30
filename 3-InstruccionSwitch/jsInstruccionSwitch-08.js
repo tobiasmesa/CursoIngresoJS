a56ac4a3ed60794a9b28b58aa998deb0946ada6a
tobias <tobiasmesa030303@gmail.com>
2022-01-30 19:48:20 -0300

    Switch - Ej 8 re-solved

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,23 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	var destinoIngresado = document.getElementById("txtIdDestino").value;
+	let temperatura;
+	
+	switch (destinoIngresado){ 
+		case "Bariloche": 
+			temperatura = "frio";
+			break;
+		case "Cataratas":
+			temperatura = "calor";
+			break;
+		case "Mar del plata":
+			temperatura = "calor";
+			break;
+		case "Ushuaia":
+			temperatura = "frio";
+			break;
+	}
+
+	alert("En el destino ingresado hace " + temperatura);
 
 }//FIN DE LA FUNCIÓN
