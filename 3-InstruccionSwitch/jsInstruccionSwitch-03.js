3859138ab090b1cf5f6c8acd032992b2a2e65b8d
tobias <tobiasmesa030303@gmail.com>
2022-01-30 12:33:32 -0300

    Switch - Ej.3 solved

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -1,10 +1,15 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes = document.getElementById("txtIdMes").value;
 	
-	
+	switch(mes){
+		case "Febrero": 
+			alert("Este mes no tiene más de 29 días.");
+			break;
+		default:
+			alert("Este mes tiene mas de 30 o más días.");
+			break;
+	}
 
 
 }//FIN DE LA FUNCIÓN
