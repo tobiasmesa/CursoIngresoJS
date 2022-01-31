7fa827afc2e148a0479d15f129a659b66624d270
tobias <tobiasmesa030303@gmail.com>
2022-01-31 15:17:38 -0300

    TP 8 - PPT 2.0 solved

--- 8-TPs/08-jsPiedarPapelTijera2.js
+++ 8-TPs/08-jsPiedarPapelTijera2.js
@@ -1,31 +1,74 @@
 var eleccionMaquina;
-var ContadorDeEmpates=0;
-var ContadorDeGanadas=0;
-var ContadorDePerdidas=0;
+var ContadorDeEmpates = 0;
+var ContadorDeGanadas = 0;
+var ContadorDePerdidas = 0;
+
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 3
-	 	
+	let random = Math.round(Math.random() * 2 + 1);
+    document.getElementById("txtIdGanadas").value = ContadorDeGanadas + " ganadas";
+	document.getElementById("txtIdEmpatadas").value = ContadorDeEmpates  + " empates";
+	document.getElementById("txtIdPerdidas").value = ContadorDePerdidas  + " perdidas";
+	
+	
+	switch (random) {
+        case 1:
+            eleccionMaquina = "piedra"; 
+            break;
+        case 2: 
+            eleccionMaquina = "papel"; 
+            break;
+        case 3:
+            eleccionMaquina = "tijera"; 
+            break;
+    }
 
 
 
+//alert(eleccionMaquina)
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
-
+	if (eleccionMaquina == "piedra") {
+        alert("Empate");
+		ContadorDeEmpates++;
+    } else if (eleccionMaquina == "tijera") {
+        alert("Ganó");
+		ContadorDeGanadas++;
+    }  else {
+        alert("Perdió");
+		ContadorDePerdidas++;
+    }
+    comenzar();
 }//FIN DE LA FUNCIÓN
 function papel()
 {
+    if (eleccionMaquina == "papel") {
+        alert("Empate");
+		ContadorDeEmpates++;
+    } else if (eleccionMaquina == "piedra") {
+        alert("Ganó");
+		ContadorDeGanadas++;
+    }  else {
+        alert("Perdió");
+		ContadorDePerdidas++;
+    }
+    comenzar();
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
+	if (eleccionMaquina == "tijera") {
+        alert("Empate");
+		ContadorDeEmpates++;
+    } else if (eleccionMaquina == "papel") {
+        alert("Ganó");
+		ContadorDeGanadas++;
+    }  else {
+        alert("Perdió");
+		ContadorDePerdidas++;
+    }
+
+    comenzar();
 }//FIN DE LA FUNCIÓN
-
-function mostrarResultado()
-{
-
-}
