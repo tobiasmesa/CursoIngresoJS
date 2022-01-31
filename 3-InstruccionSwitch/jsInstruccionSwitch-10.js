46b8b44934b149390a7e31fd46662b6085aaf565
tobias <tobiasmesa030303@gmail.com>
2022-01-30 21:07:29 -0300

    Switch - Ej. 10 solved

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,32 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	var estacionIngresada =document.getElementById("txtIdEstacion").value;
+	var destinoIngresado = document.getElementById("txtIdDestino").value;
+	switch (estacionIngresada) {
+		case "Invierno": 
+			if(destinoIngresado == "Bariloche"){ 
+				alert("Se viaja.");
+			} else {
+				alert("No se viaja.");
+			}
+			break;
+		case "Verano": 
+			if(destinoIngresado == "Mar del plata" || destinoIngresado == "Cataratas") {
+				alert("Se viaja.");
+			} else {
+				alert("No se viaja.");
+			}
+			break;
+		case "Primavera":
+			if(destinoIngresado != "Bariloche"){
+				alert("Se viaja.");
+			} else {
+				alert("No se viaja.");
+			}
+			break;
+		default:
+			alert("Se viaja.");
+		break;
+	}
 
 }//FIN DE LA FUNCIÓN
