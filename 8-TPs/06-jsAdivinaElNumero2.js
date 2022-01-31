5a7c5f2b2ee063bcb846dbfc13a1d757a4c3c464
tobias <tobiasmesa030303@gmail.com>
2022-01-31 14:51:25 -0300

    TP 6 - Adivinaelnumero 2.0 solved

--- 8-TPs/06-jsAdivinaElNumero2.js
+++ 8-TPs/06-jsAdivinaElNumero2.js
@@ -11,18 +11,56 @@ Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
 var contadorIntentos;
+const min = 1;
+const max = 100;
+
 
 function comenzar()
 {
+  document.getElementById("txtIdNumero").value = " ";
 	//Genero el número RANDOM entre 1 y 100
-	 
-	
-
+	numeroSecreto = Math.round(Math.random() * (max - min) + min);
+  alert(numeroSecreto)
+  contadorIntentos = 0;
+  document.getElementById("txtIdIntentos").value = contadorIntentos;
 }
 
 function verificar()
 {
-	
-	
-
+  contadorIntentos++;
+  let numeroIngresado = parseInt(document.getElementById("txtIdNumero").value);
+  document.getElementById("txtIdIntentos").value = contadorIntentos;
+  if (numeroIngresado == numeroSecreto) {
+		switch(contadorIntentos) {
+			case 1: 
+				alert("usted es un Psíquico");
+				break;
+			case 2: 
+				alert("excelente percepción");
+				break;
+			case 3: 
+				alert("Esto es suerte");
+				break;
+			case 4: 
+				alert("Excelente técnica");
+				break;
+			case 5: 
+				alert("usted está en la media");
+				break;
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+				alert("falta técnica");
+				break;
+			default:
+				alert("afortunado en el amor!!")
+				break;
+		}
+  } else if (numeroIngresado < numeroSecreto) {
+    alert("Falta"); 
+  } else {
+    alert ("Se pasó");
+  }
 }
