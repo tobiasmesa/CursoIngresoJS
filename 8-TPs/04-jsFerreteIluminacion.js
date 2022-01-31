3f4f23133bae9a0f998cde377d6ec2af5c7500cb
tobias <tobiasmesa030303@gmail.com>
2022-01-31 13:53:23 -0300

    TP 4 - Ferrete Iluminacion solved

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -4,16 +4,15 @@ A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%.
 B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
 C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
 D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
-E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
- ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
 function CalcularPrecio () 
 {
  	let cantidadLamparas = parseInt(document.getElementById("txtIdCantidad").value);
     let marca = document.getElementById("Marca").value;
-    const lampara = 65;
+    const lampara = 35;
     let total = lampara * cantidadLamparas;
+    let impuesto; 
 
     switch (cantidadLamparas) {
         case 5: 
@@ -35,15 +34,29 @@ function CalcularPrecio ()
                 total = (total) - (total) * 0.15; 
             } else if (marca == "FelipeLamparas") {
                 total = (total) - (total) * 0.15; 
+            } else {
+                total = (total) - (total) * 0.05; 
             }
             break;
-        case 2: 
-
+        case 2:
+        case 1:
+            break; 
         default: 
             total = (cantidadLamparas * 0.5) * lampara;
         break;
     }
 
+
+    /* E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar
+     del impuesto con el siguiente mensaje:
+ ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
+*/
+
+    if(total > 120) {
+        impuesto = total * 0.1;
+        total = total + impuesto;
+        alert(`Usted pago ${impuesto} de IIBB.`)
+    }
     document.getElementById("txtIdprecioDescuento").value = "$" + total;
 
 }
